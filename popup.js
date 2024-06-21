21e86de8382b92f9501f72361b6e7eb8a563f187
Josue Vega <j.vegav@uniandes.edu.co>
2024-06-21 14:52:27 -0500

    Commit final

---
+++ popup.js
@@ -0,0 +1,40 @@
+
+const generateHTML = () => {
+    return `<div id = "principal">
+                <h1> No puedes entrar lo siento ve a estudiar </h1>
+            </div>`
+}
+
+const generateStyle = () => {
+    return `body {
+                background : white;
+            }
+            .principal {
+                align-items: center;
+                justify-content: center;
+                display: flex;
+                color : black ;
+                font-family: Arial, Helvetica, sans-serif;
+            }`
+}
+switch (window.location.hostname){
+    case "www.youtube.com":
+        document.body.innerHTML = generateHTML();
+        document.head.innerHTML = generateStyle();
+        break;
+    case "www.facebook.com":
+        document.body.innerHTML = generateHTML();
+        document.head.innerHTML = generateStyle();
+        break;
+    case "www.instagram.com":
+        document.body.innerHTML = generateHTML();
+        document.head.innerHTML = generateStyle();
+        break;
+    case "www.youtube.com":
+        document.body.innerHTML = generateHTML();
+        document.head.innerHTML = generateStyle();
+        break;
+    
+    
+        
+}
