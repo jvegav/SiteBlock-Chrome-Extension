325ece1c2d676f69680f9289194dd29f6bf524eb
Josue Vega <j.vegav@uniandes.edu.co>
2024-06-23 13:50:04 -0500

    add and delete urls complete

--- simple-chrome-extension/src/App.jsx
+++ simple-chrome-extension/src/App.jsx
@@ -1,42 +1,41 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
  
-  const [ tabs, setTabs] = useState(['https://youtube.com'])
-
-  function changeInfo(tab)  {
-        if (tab) {
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id},
-                
-                func: () => {
-                  document.body.innerHTML = `<div id = "principal">
-              <h1> No puedes entrar lo siento ve a estudiar </h1>
-          </div>`;
-                  
-                }
-            });
-        } else {
-            console.error('No active tab found');
-        }
-};
-
-const onClick = () => {
-
-}
+  const [ tabs, setTabs] = useState(()=>{
+    const localValue = localStorage.getItem("TABS");
+    if(localValue ===null) return []
+    return JSON.parse(localValue)
+  })
 
 
-const onSumbit = (event) => {
+  useEffect(() => {
+    localStorage.setItem("TABS", JSON.stringify(tabs))
+  },[tabs])
+
+
+  const[newURL, setNewURL] =  useState("")
+
+  const onClick = () => {
+
+  }
+
+
+  function addURL(event){
     event.preventDefault()
-    const url = event.target.url.value 
-    if(url) {
-      setTabs(prevTabs => [...prevTabs,url])
-      event.target.url.value
-    }
-}
+    setTabs(prevTabs => [...prevTabs, {id: crypto.randomUUID(),url:newURL}])
+    setNewURL("")
+  }
+
+  const deleteURL = (id) => {
+    setTabs(currentTabs => {
+      return currentTabs.filter(tab => tab.id !== id);
+    })
+  }
+
 
   return (
     <>
@@ -45,24 +44,44 @@ const onSumbit = (event) => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
+
       <h1>Vite + React</h1>
+
+
       <div className="card">
-        <button  id='myButton' onClick={onClick}>
+        <button  id='myButton' className='button' onClick={onClick}>
           Block Sites
         </button>
       </div>
+
+
+
       <div className='pb-8'>
-        <form id="blockForm" onSubmit={onSumbit}>
+        <form id="blockForm" onSubmit={addURL}>
           <div className='pb-4'>
-          <input type="text" id="url" placeholder="Enter URL to block" required className='justify-center items-center text-center rounded-lg h-10 '/>
+            <input value={newURL} type="text" id="url" onChange={e => setNewURL(e.target.value)} required className='justify-center items-center text-center rounded-lg h-10 '/>
           </div>
+
           <button  className = 'button-2'type="submit">Add URL</button>
         </form>
       </div>
+
+
       
       <ul id="blockedList" >
-        {tabs.map((tab, index) => (
-          <li className='text-xl font-bold' key={index}>{tab}</li>
+        {tabs.map((tab) => (
+          
+          <li className='text-xl font-bold mt-2' key={tab.id}>
+            {tab.url}
+            
+            <button className='button-delete' onClick={() => deleteURL(tab.id)}>
+               Delete
+            </button>
+            
+            
+          </li>
+
         ))}
       </ul>
 
