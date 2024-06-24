50eed67d7beb1272e1bf127d08b1f35965360d99
Josue Vega <j.vegav@uniandes.edu.co>
2024-06-24 12:19:37 -0500

    changes for publishing

--- simple-chrome-extension/src/App.jsx
+++ simple-chrome-extension/src/App.jsx
@@ -1,106 +1,86 @@
-
-import { useEffect, useState } from 'react';
-import './App.css'
-import useAlert from './hooks/useAlert';
-import Logo from './components/Logo';
-import BlockBar from './components/BlockBar';
-import Form from './components/Form';
-import List from './components/List';
-import Blocked from './components/Blocked';
+import { useEffect, useState } from "react";
+import "./App.css";
+import useAlert from "./hooks/useAlert";
+import Logo from "./components/Logo";
+import BlockBar from "./components/BlockBar";
+import Form from "./components/Form";
+import List from "./components/List";
+import Blocked from "./components/Blocked";
 
 function App() {
+	const [tabs, setTabs] = useState([]);
 
-  const [ tabs, setTabs] = useState([])
+	const [blocked, setBlocked] = useState(false);
 
-  const [blocked, setBlocked] = useState(false)
+	const [addUrl, setAddUrl] = useState(false);
 
-  const[addUrl, setAddUrl] = useState(false);
+	useEffect(() => {
+		chrome.storage.local.get(["blocked", "blockedURLS"], function (result) {
+			if (result.blocked !== undefined) {
+				setBlocked(result.blocked);
+			}
+			if (result.blockedURLS) {
+				setTabs(result.blockedURLS);
+			}
+		});
+	}, []);
 
-  useEffect(() => {
-    chrome.storage.local.get(['blocked','blockedURLS'], function(result) {
-      if (result.blocked !== undefined) {
-        setBlocked(result.blocked);
-      }
-      if(result.blockedURLS) {
-        setTabs(result.blockedURLS)
-      }
-    })
-  },[])
+	useEffect(() => {
+		chrome.storage.local.set({ blocked });
+	}, [blocked]);
 
-  useEffect(() => {
-    chrome.storage.local.set({ blocked });
-  }, [blocked]);
+	function blockSites(value) {
+		chrome.runtime.sendMessage({ action: "toggleBlocking", value });
+		setBlocked(true);
+	}
 
+	function unblockSites(value) {
+		chrome.runtime.sendMessage({ action: "toggleBlocking", value });
+		setBlocked(false);
+	}
 
+	function addURL(newURL) {
+		const updatedTabs = [...tabs, { id: crypto.randomUUID(), url: newURL }];
+		setTabs(updatedTabs);
 
-  function blockSites (value) {
-    chrome.runtime.sendMessage({ action: "toggleBlocking",value });
-    setBlocked(true)
-    
-  }
+		chrome.storage.local.set({ blockedURLS: updatedTabs }, function () {
+			chrome.runtime.sendMessage({ action: "updateBlockedURLS" });
+		});
+	}
 
+	const deleteURL = (id) => {
+		const newTabs = tabs.filter((tab) => tab.id !== id);
+		setTabs(newTabs);
 
+		chrome.storage.local.set({ blockedURLS: newTabs }, function () {
+			chrome.runtime.sendMessage({ action: "updateBlockedURLS" });
+		});
+	};
 
-  function unblockSites (value) {
-    
-    chrome.runtime.sendMessage({ action: "toggleBlocking",value });
-    setBlocked(false)
-    
-  }
+	const handleAddUrl = () => {
+		setAddUrl(!addUrl);
+	};
 
+	return (
+		<>
+			<Logo />
 
+			<h1 className="font-mono text-center">BlockIt</h1>
 
-  function addURL(newURL){
-    const updatedTabs = [...tabs, {id: crypto.randomUUID(),url:newURL}]
-    setTabs(updatedTabs)
+			<Blocked blocked={blocked} />
 
-    chrome.storage.local.set({blockedURLS :updatedTabs},function(){
-      chrome.runtime.sendMessage({action : "updateBlockedURLS"})
-    })
-  }
+			<BlockBar blockSites={blockSites} unblockSites={unblockSites} />
 
-
-
-  const deleteURL = (id) => {
-    const newTabs = tabs.filter(tab => tab.id !== id)
-    setTabs(newTabs)
-
-    chrome.storage.local.set({blockedURLS :  newTabs}, function(){
-      chrome.runtime.sendMessage({action : "updateBlockedURLS"})
-    })
-  }
-
-  const handleAddUrl = () =>{
-    setAddUrl(!addUrl)
-  }
-
-
-  return (
-    <>
-      <Logo/>
-
-      <h1 className='font-mono text-center'>BlockIt</h1>
-
-      
-      <Blocked
-      blocked={blocked}/>
-
-      <BlockBar
-      blockSites={blockSites}
-      unblockSites={unblockSites}/>
-
-      <button onClick={handleAddUrl} className='button-2 text-center justify-center items-center flex m-auto ' >
-        {addUrl ? 'Hide Form' : 'Add Url'}
-      </button>
-      {addUrl && <Form addURL={addURL} /> }
-      {addUrl && <List deleteURL={deleteURL} tabs={tabs}/> }
-      
-
-
-      
-
-    </>
-  )
+			<button
+				onClick={handleAddUrl}
+				className="button-2 text-center justify-center items-center flex m-auto "
+			>
+				{addUrl ? "Hide Form" : "Add Url"}
+			</button>
+			{addUrl && <Form addURL={addURL} />}
+			{addUrl && <List deleteURL={deleteURL} tabs={tabs} />}
+		</>
+	);
 }
 
-export default App
+export default App;
