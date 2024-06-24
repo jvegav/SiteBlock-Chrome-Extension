8e2fffb94a8de1161f290b0c8429062401b96111
Josue Vega <j.vegav@uniandes.edu.co>
2024-06-24 15:36:58 -0500

    change icons

--- blockit/src/components/Logo.jsx
+++ blockit/src/components/Logo.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 
-import blockit_logo from "../assets/icon-48.png";
+import blockit_logo from "../assets/icon-128.png";
 
 const Logo = () => {
-	return (
-		<div className="flex justify-center">
-			<img
-				src={blockit_logo}
-				className="logo blockit w-[150px] h-[150px]"
-				alt="BlockIt logo"
-			/>
-		</div>
-	);
+	return <div className="flex justify-center"></div>;
 };
 
 export default Logo;
